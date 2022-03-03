acb064d78ce15e268773782d4be5be6e601f7b7c
Ikaros <327209194@qq.com>
2022-03-03 10:45:39 +0800

    新增语言切换菜单功能；补充情报和图片

---
+++ js/index.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    console.log("welcome");
+}
+
+// 语言切换
+function language_change(language_val) {
+    var a_tag = document.getElementsByTagName("a");
+    var len = a_tag.length - 3;
+    var language_index = 0;
+
+    // 中 日 英
+    var arr = [
+        ["首页", "B站主页", "QQ群", "表情包", "特殊情报", "特典", "发病语录", "舰长视频", "chiikawa套图", "关于", "Language"],
+        ["トップページ", "bilibili", "QQぐん", "表情パック", "じょうほう", "特典", "発症語録", "艦長ビデオ", "chiikawa", "について", "Language"],
+        ["HomePage", "bilibili", "QQ Group", "Emoticon", "Information", "Privilege", "GroupQuotations", "CaptainVideo", "chiikawa", "About", "Language"],
+    ];
+
+    if(language_val == "ch") language_index = 0;
+    else if(language_val == "jp") language_index = 1;
+    else if(language_val == "en") language_index = 2;
+    else language_index = 2;
+    for(var i = 0; i < len; i++) {
+        a_tag[i].innerText = arr[language_index][i];
+    }
+}
