c9a338a16289dccb8fb77cc78a106a16852f970d
Ikaros <327209194@qq.com>
2022-03-04 13:58:07 +0800

    补充特典描述

--- js/privilege.js
+++ js/privilege.js
@@ -3,10 +3,10 @@ window.onload = function () {
     var p_explain = document.getElementById("p_explain");
 
     if(language == "ch") {
-        p_explain.innerText = "上舰后方可得到大小姐亲笔签名插画特典~";
+        p_explain.innerText = "上舰后方可得到大小姐亲笔签名插画特典~<br>特典每个月都有，提前续舰的晚点会发。";
     } else if(language == "jp") {
-        p_explain.innerText = "艦に乗ってからお嬢様直筆サイン入りイラスト特典付き~";
+        p_explain.innerText = "艦に乗ってからお嬢様直筆サイン入りイラスト特典付き~<br>特典は毎月ありますが、早めの続艦の遅れが出ます。";
     } else {
-        p_explain.innerText = "After boarding the ship, you can get the special illustration signed by the eldest lady~";
+        p_explain.innerText = "After boarding the ship, you can get the special illustration signed by the eldest lady~<br>Special codes are available every month, and ship renewal in advance will be issued later.";
     }
 }
