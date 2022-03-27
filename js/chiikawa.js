2f013a32d885f57ac644e3ec7ae9332d00a8cb6a
Ikaros <327209194@qq.com>
2022-03-27 17:11:02 +0800

    添加chiikawa表情包

--- js/chiikawa.js
+++ js/chiikawa.js
@@ -1,7 +1,65 @@
+/*
+window.onload = function() {
+    imgLocation("container", "box");
+}
+*/
+
 // 瀑布流参考：https://www.jb51.net/article/90349.htm
 
 window.onload = function() {
-    imgLocation("container", "box");
+	// var imgData = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg",
+    //     "11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg",
+    //     "21.jpg","22.jpg","23.jpg","24.jpg","25.jpg","26.jpg","27.jpg","28.jpg","29.jpg","30.jpg",
+    //     "1.gif","2.gif","3.gif","4.gif","5.gif","6.gif",
+    //     "1.png","2.png","3.png","4.png","5.png","6.png","7.png"];
+
+	var jpg_num = 4;
+    var png_num = 136;
+
+    var cparent = document.getElementById("container");
+    for (var i = png_num; i > 0; i--) {
+        var ccontent = document.createElement("div");
+        ccontent.className = "box";
+        
+        var boximg = document.createElement("div");
+        boximg.className = "box_img";
+        
+        var img = document.createElement("img");
+        img.src = "img/chiikawa/" + i + ".png";
+
+        boximg.appendChild(img);
+        ccontent.appendChild(boximg);
+        cparent.appendChild(ccontent);
+    }
+
+	for (var i = jpg_num; i > 0; i--) {
+        var ccontent = document.createElement("div");
+        ccontent.className = "box";
+        
+        var boximg = document.createElement("div");
+        boximg.className = "box_img";
+        
+        var img = document.createElement("img");
+        img.src = "img/chiikawa/" + i + ".jpg";
+
+        boximg.appendChild(img);
+        ccontent.appendChild(boximg);
+        cparent.appendChild(ccontent);
+    }
+
+    // imgLocation("container", "box");
+}
+
+function checkFlag() {
+	var cparent = document.getElementById("container");
+	var ccontent = getChildElement(cparent, "box"); //图片的所有box数
+	var lastContentHeight = ccontent[ccontent.length - 1].offsetTop; //最后一张图片距离顶部高度
+	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop; //滚动条距离顶部高度
+	var pageHeight = document.documentElement.clientHeight || document.body.clientHeight; //屏幕高度
+	// console.log(lastContentHeight+","+scrollTop+","+pageHeight);
+	if (lastContentHeight < scrollTop + pageHeight) {
+		return true;
+	}
 }
 
 function imgLocation(parent, content) {
